556083ab697b477de02553a01af7fb9f18aaa03f
Miquel Angel Fontana <miquel.angel.f.t@gmail.com>
2019-04-01 23:38:39 +0200

    exercise 6

--- exercises/src/exercise6.js
+++ exercises/src/exercise6.js
@@ -3,4 +3,12 @@ import _ from 'lodash';
 import {getHabilityValueForCharacter} from "./exercise4";
 import {getTotalDamage} from "./exercise5";
 
-export const getTotalDamageForCharacter = '';
+export const getTotalDamageForCharacter = (team, character, attacks) => {
+    const getBaseDamage = _.partialRight(getHabilityValueForCharacter, 'baseDamage');
+    const applyAttacks = _.partial(getTotalDamage, attacks);
+
+    return fp.compose(
+        applyAttacks,
+        getBaseDamage
+    )(team, character);
+};
