b43d7160d3a9b66be328ffca3635342f397ef2c5
Miquel Angel Fontana <miquel.angel.f.t@gmail.com>
2019-04-01 23:03:16 +0200

    exercise 3

--- exercises/src/exercise3.js
+++ exercises/src/exercise3.js
@@ -1,2 +1,10 @@
 
-export const transformInRageMode = '';
+export const transformInRageMode = (character) => {
+  let characterRange = {};
+
+    Object.keys(character).forEach((key) => {
+        characterRange[key] = Math.floor(character[key] + 0.1 * character[key]);
+    });
+
+  return characterRange;
+};
