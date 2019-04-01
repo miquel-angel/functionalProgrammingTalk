4e338bbaadcd901b64abb40d818f013f6c043e28
Miquel Angel Fontana <miquel.angel.f.t@gmail.com>
2019-04-01 22:52:50 +0200

    exercise 1

--- exercises/src/exercise1.js
+++ exercises/src/exercise1.js
@@ -1,3 +1,20 @@
-export function addWeaponDamage(weapon, baseDamage) {
+const damagesPerWeapon = [
+    {
+        'name': 'sword',
+        'damage': 10
+    },
+    {
+        'name': 'dagger',
+        'damage': 5
+    },
+    {
+        'name': 'arch',
+        'damage': 7
+    }
+];
 
+export function addWeaponDamage(weapon, baseDamage) {
+    const weaponDamage = damagesPerWeapon.filter((weaponDamage) => weaponDamage.name === weapon);
+
+    return (weaponDamage[0])? weaponDamage[0].damage + baseDamage : baseDamage;
 }
