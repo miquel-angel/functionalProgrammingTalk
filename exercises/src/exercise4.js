98940a7548eeded3d67bb37ee468c0ab02b60b99
Miquel Angel Fontana <miquel.angel.f.t@gmail.com>
2019-04-01 23:06:33 +0200

    exercise 4

--- exercises/src/exercise4.js
+++ exercises/src/exercise4.js
@@ -1,3 +1,5 @@
 import _ from 'lodash';
 
-export const getHabilityValueForCharacter = '';
+export const getHabilityValueForCharacter = _.curry((team, characterName, hability) => {
+    return team[characterName][hability];
+});
