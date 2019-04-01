4e13b245fce77e82fa6ea960acbbbf71bbdb7493
Miquel Angel Fontana <miquel.angel.f.t@gmail.com>
2019-04-01 22:43:12 +0200

    exercise 4

---
+++ exercises/test/exercise4Test.js
@@ -0,0 +1,40 @@
+import assert from 'assert';
+import {getHabilityValueForCharacter} from "../src/exercise4";
+
+/**
+ * See the tests and what return in each moment.
+ */
+
+const characterJon = {
+
+    'baseDamage': 10,
+    'agility': 30,
+    'defense': 20
+};
+
+const characterArya = {
+
+    'baseDamage': 20,
+    'agility': 50,
+    'defense': 10
+};
+
+const team = {
+    'Jon': characterJon,
+    'Arya': characterArya
+};
+
+
+describe('Exercise 4', () => {
+    it('Given an object should return a function', () => {
+        assert.equal(true, getHabilityValueForCharacter(team) instanceof Function);
+    });
+
+    it('Given an object and a character name should return a function', () => {
+        assert.equal(true, getHabilityValueForCharacter(team, 'Arya') instanceof Function);
+    });
+
+    it('Given an object and a character name and hability should return a value', () => {
+        assert.equal(true, getHabilityValueForCharacter(team, 'Arya', 'baseDamage') instanceof Function);
+    });
+});
