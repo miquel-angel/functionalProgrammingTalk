429f94cb0fcc48699e10119af5d411377a3b1aee
Miquel Angel Fontana <miquel.angel.f.t@gmail.com>
2019-04-02 13:52:34 +0200

    improve names

--- exercises/src/exercise5.js
+++ exercises/src/exercise5.js
@@ -2,11 +2,11 @@
 export const getTotalDamage = (attacks, baseDamage) => {
     return attacks
         .map((attack) => attack(baseDamage))
-        .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+        .reduce((accumulatedDamage, currentDamage) => accumulatedDamage + currentDamage, 0);
 };
 
 export const getRemainingLive = (attacks, baseDamage, initialLive) => {
     return attacks
         .map((attack) => attack(baseDamage))
-        .reduce((previousValue, currentValue) => previousValue - currentValue, initialLive);
+        .reduce((remainingLive, currentDamage) => remainingLive - currentDamage, initialLive);
 };
